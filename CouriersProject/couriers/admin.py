b604c529a6a33eaffa103e2cbee35a0abfae4658
Vatsek Pavel <pvatsek@gmail.com>
2024-04-25 21:26:40 +0300

    change statistics

--- CouriersProject/couriers/admin.py
+++ CouriersProject/couriers/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Courier, Motivation, CourierСard
+from .models import Courier, Motivation, CourierCard
 
 
 @admin.register(Courier)
@@ -14,8 +14,9 @@ class MotivationAdmin(admin.ModelAdmin):
     list_display = ('title', 'base_rate', 'included_deliveries', 'rate_for_delivery')
 
 
-@admin.register(CourierСard)
+@admin.register(CourierCard)
 class CourierCardAdmin(admin.ModelAdmin):
     list_display = ('date', 'courier', 'count_deliveries')
+    # list_display = ('date', 'count_deliveries')
     search_fields = ['date']
     search_help_text = 'Поиск по дате'
